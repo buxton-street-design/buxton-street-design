5ab983bbb062163c9d2f143572f389dbcf206cfa
topher-churnick <topher.churnick@gmail.com>
2014-05-09 15:24:58 -0400

    CSC_commit

--- gitcommit.sh
+++ gitcommit.sh
@@ -21,3 +21,22 @@ git push origin master
 #Check the git log for the win!
 
 git lg
+
+
+#Switch to /site directory
+
+cd /_site
+
+
+#Do it all over again
+
+
+
+git status --long
+git add -A
+git status --long
+git commit -a -m "CSC_commit"
+git push origin master
+git lg
+
+
