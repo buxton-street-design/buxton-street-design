550575b224b948d6d3a9cb21fb43bbc516bf42b4
topher-churnick <topher.churnick@gmail.com>
2014-05-09 14:28:57 -0400

    CSC_commit

---
+++ gitcommit.sh
@@ -0,0 +1,23 @@
+#First I want to check status...
+
+git status --long
+
+#run add...
+
+git add -A
+
+#Check status...
+
+git status --long
+
+#commit with message...
+
+git commit -a -m "CSC_commit"
+
+#push to origin...
+
+git push origin master
+
+#Check the git log for the win!
+
+git lg
